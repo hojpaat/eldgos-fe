d9bfc236324db84956a066aff52b9be2ab90c796
hojpaat <judit.of.horvath@gmail.com>
2024-12-15 21:12:47 +0000

    add react routing to the project

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import HomePage from './HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DrawerAppBar from './Nav'
+import Test from './Test';
+import FunFact from './FunFact';
+
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <DrawerAppBar />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/test' element={<Test />} />
+                <Route path='/fun-fact' element={<FunFact />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
